c2e6eee1937bebba07782fc2828f86e92858cd2e
Eunwoo <dmsdn960@gmail.com>
2019-11-07 01:40:18 +0900

    새로운 영화리스트 만들기

--- movie.js
+++ movie.js
@@ -1,3 +1,16 @@
 const data = require("./data");
 const { linkArray, funcs } = require("./array");
 linkArray(funcs);
+
+const movieList = data.customMap(({ id, title, year, rating }) => ({
+  id,
+  title,
+  year,
+  rating
+}));
+
+function leastRatingList(rate) {
+  return movieList.customFilter(({ rating }) => rating >= rate);
+}
+
+console.log(leastRatingList(5));
