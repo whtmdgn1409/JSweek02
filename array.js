e4ec4787a75dd046796777400f3e9a7a525b8fab
Eunwoo <dmsdn960@gmail.com>
2019-11-07 01:02:18 +0900

    map 함수 구현

--- array.js
+++ array.js
@@ -52,12 +52,28 @@ function customForEach(callbackfn, thisArg) {
   }
 }
 
+function customMap(callbackfn, thisArg) {
+  const array = this;
+  const length = array.length;
+  if (thisArg) {
+    callbackfn = callbackfn.bind(thisArg);
+  }
+  const result = [];
+  for (let index = 0; index < length; index++) {
+    const currentValue = array[index];
+    result.push(callbackfn(currentValue, index, array));
+  }
+  return result;
+}
+
 Array.prototype.customFilter = customFilter;
 Array.prototype.customEvery = customEvery;
 Array.prototype.customSome = customSome;
 Array.prototype.customForEach = customForEach;
+Array.prototype.customMap = customMap;
 
 console.log([1, 2, 3, 4, 5].customFilter(v => v > 2));
 console.log([1, 2, 3, 4, 5].customEvery(v => v > 0));
 console.log([1, 2, 3, 4, 5].customSome(v => v == 1));
 [1, 2, 3, 4, 5].customForEach(v => console.log(v));
+console.log([1, 2, 3, 4, 5].customMap(v => v * v));
