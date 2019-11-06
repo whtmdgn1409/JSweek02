5f5754d3bad798a5a55a38f439d32d6fb47900f4
Eunwoo <dmsdn960@gmail.com>
2019-11-07 01:01:10 +0900

    forEach 함수 구현

--- array.js
+++ array.js
@@ -40,10 +40,24 @@ function customSome(callbackfn, thisArg) {
   return result;
 }
 
+function customForEach(callbackfn, thisArg) {
+  const array = this;
+  const length = array.length;
+  if (thisArg) {
+    callbackfn = callbackfn.bind(thisArg);
+  }
+  for (let index = 0; index < length; index++) {
+    const currentValue = array[index];
+    callbackfn(currentValue, index, array);
+  }
+}
+
 Array.prototype.customFilter = customFilter;
 Array.prototype.customEvery = customEvery;
 Array.prototype.customSome = customSome;
+Array.prototype.customForEach = customForEach;
 
 console.log([1, 2, 3, 4, 5].customFilter(v => v > 2));
 console.log([1, 2, 3, 4, 5].customEvery(v => v > 0));
 console.log([1, 2, 3, 4, 5].customSome(v => v == 1));
+[1, 2, 3, 4, 5].customForEach(v => console.log(v));
