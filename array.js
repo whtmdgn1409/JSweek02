282367f8451cced07b350f7bbcee6450004a7692
Eunwoo <dmsdn960@gmail.com>
2019-11-07 00:55:49 +0900

    every 함수 구현

--- array.js
+++ array.js
@@ -1,16 +1,33 @@
 function customFilter(callbackfn, thisArg) {
-  const length = this.length;
+  const array = this;
+  const length = array.length;
   if (thisArg) {
     callbackfn = callbackfn.bind(thisArg);
   }
   const result = [];
   for (let index = 0; index < length; index++) {
-    const element = this[index];
-    callbackfn(element, index, this) && result.push(element);
+    const element = array[index];
+    callbackfn(element, index, array) && result.push(element);
+  }
+  return result;
+}
+
+function customEvery(callbackfn, thisArg) {
+  const array = this;
+  const length = array.length;
+  if (thisArg) {
+    callbackfn = callbackfn.bind(thisArg);
+  }
+  let result = true;
+  for (let index = 0; index < length; index++) {
+    const currentValue = array[index];
+    result = result && callbackfn(currentValue, index, array);
   }
   return result;
 }
 
 Array.prototype.customFilter = customFilter;
+Array.prototype.customEvery = customEvery;
 
 console.log([1, 2, 3, 4, 5].customFilter(v => v > 2));
+console.log([1, 2, 3, 4, 5].customEvery(v => v > 0));
