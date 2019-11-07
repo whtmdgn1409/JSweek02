2f8e579aaff1eff225947460edf35b91654f1d42
pongsoyun <thdbstjdud@gmail.com>
2019-11-07 19:20:57 +0900

    customEvery finished

--- customFunction.js
+++ customFunction.js
@@ -15,3 +15,26 @@ Array.prototype.customFilter = function(func) {
 
 var filtered = [12, 5, 8, 130, 45].customFilter( value => value>10 );
 console.log(filtered);
+
+
+
+/// 2. every
+
+//original
+var ages = [32, 33, 16, 40];
+// console.log(ages.every(age => age>=13));
+
+//custom
+Array.prototype.customEvery = function(func) {
+    var result = []; 
+    var cnt = 0;
+    var len = this.length;
+    for (i = 0; i<len; i++){
+        if(func(this[i]) == true) { cnt ++; }
+    }
+    return (cnt == len ? true : false);
+}
+console.log(ages.customEvery( age => age>=13 ));
+
+
+/// 3. some
