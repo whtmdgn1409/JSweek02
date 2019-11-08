0022c3b00f34d0c41af7459ff14db8ad266cf2fc
gv0413 <ggvv0413@gmail.com>
2019-11-08 22:57:23 +0900

    customEvery 구현

--- 01_custom.js
+++ 01_custom.js
@@ -8,4 +8,12 @@ Array.prototype.customFilter = function customFilter(cb){
   return newArr;
 }
 
+Array.prototype.customEvery = function customEvery(cb){
+  for(var i=0; i < this.length; i++){
+    if(!cb(this[i])) return false;
+  }
+  return true;
+}
+
 console.log([2,3,14,45,56].customFilter(e=>e>46));
+console.log([2,3,14,45,56].customEvery(e=>e>=2));
