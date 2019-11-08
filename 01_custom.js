126b418093bc08cae056bcdb8e558024532ba12c
gv0413 <ggvv0413@gmail.com>
2019-11-08 22:45:08 +0900

    customFilter protochain연결

--- 01_custom.js
+++ 01_custom.js
@@ -1,17 +1,11 @@
-function customFilter(arr, filterFunction){
+Array.prototype.customFilter = function customFilter(cb){
   var newArr = new Array();
-  for(var i=0; i < arr.length; i++){
-    if(filterFunction(arr[i])){
-      newArr.push(arr[i]);
+  for(var i=0; i < this.length; i++){
+    if(cb(this[i])){
+      newArr.push(this[i]);
     }
   }
-  console.log(newArr);
+  return newArr;
 }
 
-function filter(value) {
-  if(value >= 10 ) return true;
-  else return false;
-}
-
-var arr=[2,3,14,45,56];
-customFilter(arr,filter);
+console.log([2,3,14,45,56].customFilter(e=>e>46));
